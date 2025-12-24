a909f633fce1b9432723fcd7b521376835fe037f
astanea <adrianstanea1@gmail.com>
2025-12-24 19:38:37 +0200

    Ch9 Ex-3: implement comparison operators classic

---
+++ cpp/src/Exercises/Ch09/9-3/Person.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <print>
+#include <string>
+
+class Person {
+   public:
+    explicit Person() = default;
+    explicit Person(std::string first_name, std::string last_name);
+    explicit Person(std::string first_name, std::string last_name,
+                    std::string initials);
+
+    Person(const Person& other);                 // Copy constructor
+    Person& operator=(const Person& other);      // Copy assignment operator
+    Person(Person&& other) noexcept;             // Move constructor
+    Person& operator=(Person&& other) noexcept;  // Move assignment operator
+    ~Person();
+
+    void swap(Person& other) noexcept;
+
+    std::string get_first_name() const;
+    std::string get_last_name() const;
+    std::string get_initials() const;
+
+    void set_last_name(std::string last_name);
+    void set_first_name(std::string first_name);
+    void set_initials(std::string initials);
+
+   protected:
+    [[nodiscard]] std::string compute_initials(std::string first_name,
+                                               std::string last_name) const;
+
+   private:
+    std::string m_first_name{""};
+    std::string m_last_name{""};
+    std::string m_initials{""};
+};
+
+void swap(Person& a, Person& b) noexcept;
+
+auto operator==(const Person& lhs, const Person& rhs) -> bool;
+auto operator!=(const Person& lhs, const Person& rhs) -> bool;
+auto operator<(const Person& lhs, const Person& rhs) -> bool;
+auto operator<=(const Person& lhs, const Person& rhs) -> bool;
+auto operator>(const Person& lhs, const Person& rhs) -> bool;
+auto operator>=(const Person& lhs, const Person& rhs) -> bool;
